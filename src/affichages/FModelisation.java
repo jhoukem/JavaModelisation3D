16854ca332de982bc71a7392a793523e54af8700
unknown <jhoukem@gmail.com>
2014-11-29 14:29:39 +0100

    zoom

--- src/affichages/FModelisation.java
+++ src/affichages/FModelisation.java
@@ -1,14 +1,17 @@
 package affichages;
 
 import java.awt.Graphics;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import javax.swing.JPanel;
 
+
 import exceptions.SegmentException;
 
-public class FModelisation extends JPanel {
+public class FModelisation extends JPanel implements MouseWheelListener {
 
 	private static final long serialVersionUID = 1L;
 	int xSize, ySize;
@@ -19,10 +22,12 @@ public class FModelisation extends JPanel {
 	Face[] fces;
 	Point[] pts;
 	Segment[] sgmts;
-
+	public static int zoom=10;
 	public FModelisation() throws SegmentException {
 		try {
-		gts = new GtsReader("tie.gts");
+			
+		this.addMouseWheelListener(this);
+		gts = new GtsReader("goblet.gts");
 		pts = gts.getPoints();
 		sgmts = gts.getSegments();
 		fces = gts.getFaces();
@@ -47,8 +52,8 @@ public class FModelisation extends JPanel {
 			x= new int[3];
 			y= new int[3];
 			for(int j =0;j<3;j++){
-				x[j]= (int)(f.get(i).xpoints[j]*10+xSize/2);
-				y[j]= (int)(f.get(i).ypoints[j]*10+ySize/2);
+				x[j]= (int)(f.get(i).xpoints[j]*zoom+xSize/2);
+				y[j]= (int)(f.get(i).ypoints[j]*zoom+ySize/2);
 			}
 			g.drawPolygon(x, y, x.length);
 			//g.fillPolygon(x, y, x.length);
@@ -65,5 +70,28 @@ public class FModelisation extends JPanel {
 		}
 		res+="]";
 		return res;
+	}
+
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		int zoom= e.getWheelRotation();
+		System.out.println("entree");
+		if(zoom<0){
+			if(this.zoom+10 != 0)
+				this.zoom+=10;	
+			else
+				this.zoom+=8;	
+		}
+		else{
+			if(this.zoom-10 != 0)
+				this.zoom+=-10;
+			else
+				this.zoom+=-8;	
+		}
+			
+		
+		this.repaint();
+			
 	}
 }
